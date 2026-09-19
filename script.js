201d6d99738632e5dba02a8e877ea212252adf0d
Katie Loesch <catherine.loesch@protonmail.com>
2026-09-19 23:31:25 +0100

    add img

--- script.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('#signup-form');
-const message = document.querySelector('#form-message');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  message.textContent =
-    'Thanks. I will send you a note when the portfolio is live.';
-  form.reset();
-});
